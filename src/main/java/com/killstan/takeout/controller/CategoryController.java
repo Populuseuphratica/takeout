fa18e37c2373e90301877e8afa687921610fb336
三叶树 <423431327@qq.com>
2022-10-17 16:22:01 +0800

    后台分类管理  分类列表  添加分类

--- src/main/java/com/killstan/takeout/controller/CategoryController.java
+++ src/main/java/com/killstan/takeout/controller/CategoryController.java
@@ -1,9 +1,14 @@
 package com.killstan.takeout.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.killstan.takeout.entity.po.Category;
+import com.killstan.takeout.entity.vo.ResultVo;
+import com.killstan.takeout.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -16,6 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/page")
+    public ResultVo<IPage<Category>> listCategory(Integer page,Integer pageSize){
+        Page<Category> page1 = new Page<>();
+        page1.addOrder(OrderItem.asc("sort"));
+        IPage<Category> categoryPage = categoryService.page(page1);
+
+        return ResultVo.success(categoryPage);
+    }
+
+    @PostMapping
+    public ResultVo addCategory(@RequestBody Category category){
+        categoryService.save(category);
+        return ResultVo.success(null);
+    }
 
 }
 
--- src/main/java/com/killstan/takeout/entity/po/Category.java
+++ src/main/java/com/killstan/takeout/entity/po/Category.java
@@ -1,11 +1,16 @@
 package com.killstan.takeout.entity.po;
 
-import java.time.LocalDateTime;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -23,9 +28,10 @@ public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "分类id")
+    @TableId(type = IdType.ASSIGN_ID)
     private Long categoryId;
 
-    @ApiModelProperty(value = "0：菜品分类；1：套餐分类")
+    @ApiModelProperty(value = "0：套餐分类；1：菜品分类")
     private Integer cgType;
 
     @ApiModelProperty(value = "分类名字")
@@ -35,16 +41,20 @@ public class Category implements Serializable {
     private Integer sort;
 
     @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
     @ApiModelProperty(value = "最后修正时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
     @ApiModelProperty(value = "创建人id")
-    private Integer createId;
+    @TableField(fill = FieldFill.INSERT)
+    private Long createId;
 
     @ApiModelProperty(value = "最后修正人id")
-    private Integer updateId;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateId;
 
 
 }
