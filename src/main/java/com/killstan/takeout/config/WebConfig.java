b2b9173dc9d48c48d4e8df1ce8c1b4cc6a23d148
三叶树 <423431327@qq.com>
2023-01-06 16:49:02 +0800

    增加Swagger导出接口

--- src/main/java/com/killstan/takeout/config/WebConfig.java
+++ src/main/java/com/killstan/takeout/config/WebConfig.java
@@ -2,9 +2,16 @@ package com.killstan.takeout.config;
 
 import com.killstan.takeout.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * @author Kill_Stan
@@ -33,6 +40,32 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/employee/login",
-                "/employee/logout", "/user/sendMsg", "/user/login", "/user/logout", "/user/sendMsg");
+                "/employee/logout", "/user/sendMsg", "/user/login", "/user/logout", "/user/sendMsg","/doc.html","/webjars/**","/swagger-resources","/v2/api-docs");
+    }
+
+    /**
+     * @Description: 接口文档配置
+     * @Param: []
+     * @Return: springfox.documentation.spring.web.plugins.Docket
+     * @Author Kill_Stan
+     * @Date 2023/1/6 16:35
+     */
+    @Bean
+    public Docket createRestApi() {
+        // 文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.killstan.takeout.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("外卖")
+                .version("1.0")
+                .description("外卖接口文档")
+                .build();
     }
 }
